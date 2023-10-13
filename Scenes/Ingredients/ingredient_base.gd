555a9e3b6e0b4903592e1e54cb643ffd87bf8384
SuperMatCat24 <79697349+SuperMatCat24@users.noreply.github.com>
2023-10-13 12:48:29 +0100

    Added cooking

--- Scenes/Ingredients/ingredient_base.gd
+++ Scenes/Ingredients/ingredient_base.gd
@@ -2,7 +2,12 @@ extends RigidBody2D
 class_name Ingredient
 
 @export var sprite : CompressedTexture2D
+@export var cook_time := 30.0
+@export var cook_result : PackedScene
 
+@onready var time_left := cook_time
+
+var cooking = false
 var hold = true
 
 func _integrate_forces(state):
@@ -19,9 +24,16 @@ func _integrate_forces(state):
 	state.set_transform(t)
 
 func _ready() -> void:
+	$ProgressBar.visible = false
 	$VisibleOnScreenNotifier2D.screen_exited.connect(_screen_exited)
+	$ProgressBar.set_max(cook_time)
 
 func _process(delta: float) -> void:
+	if cooking: time_left -= delta
+	$ProgressBar.set_value(time_left)
+	
+	if time_left <= 0: finished_cooking()
+	
 	if not Input.is_action_pressed("mouse_down"):
 		hold = false
 	if not hold: return
@@ -29,7 +41,25 @@ func _process(delta: float) -> void:
 	var rot = angular_velocity + axis * 200
 	angular_velocity = rot
 
+func finished_cooking():
+	if not cook_result: return
+	var ins = cook_result.instantiate()
+	get_parent().add_child(ins)
+	ins.global_position = global_position
+	ins.hold = false
+	queue_free()
+
 func _screen_exited() -> void:
 	Events.food_fell.emit()
 	Events.food_array.erase(self)
 	queue_free()
+
+func _on_body_entered(body: Node) -> void:
+	if not body.is_in_group("CookArea"): return
+	$ProgressBar.visible = true
+	cooking = true
+
+func _on_body_exited(body: Node) -> void:
+	if not body.is_in_group("CookArea"): return
+	$ProgressBar.visible = false
+	cooking = false
