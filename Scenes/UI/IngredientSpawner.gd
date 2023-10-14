1376b57dc8a553c02c8567293723c35a41d7556f
SuperMatCat24 <79697349+SuperMatCat24@users.noreply.github.com>
2023-10-14 09:55:07 +0100

    Added camera movement

--- Scenes/UI/IngredientSpawner.gd
+++ Scenes/UI/IngredientSpawner.gd
@@ -10,3 +10,15 @@ func _unhandled_input(event: InputEvent) -> void:
 			spawn.global_position.y = 100
 			spawn.global_position.x = get_tree().current_scene.get_global_mouse_position().x
   
+func _on_left_pressed() -> void:
+	press_key("ui_left")
+
+func _on_right_pressed() -> void:
+	press_key("ui_right")
+
+func press_key(key: String):
+	var cancel_event = InputEventAction.new()
+	cancel_event.action = key
+	cancel_event.pressed = true
+	Input.parse_input_event(cancel_event)
+
--- Scenes/UI/RoomCamera.gd
+++ Scenes/UI/RoomCamera.gd
@@ -1,10 +1,17 @@
 extends Node2D
 
+var screenwidth = ProjectSettings.get_setting("display/window/size/viewport_width")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$Camera2D.get_si
+	$Camera2D.get_screen_center_position()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func  _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_right"):
+		global_position.x += screenwidth
+	if event.is_action_pressed("ui_left"):
+		global_position.x -= screenwidth
